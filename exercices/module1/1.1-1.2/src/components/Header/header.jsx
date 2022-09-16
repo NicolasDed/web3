00e28334cae3ef66f0210aaf78cbe5036eb3c918
NicolasDed <nicolas.dedoyard@student.vinci.be>
2022-09-16 15:53:10 +0200

    1.1-1.2 import d'image

--- exercices/module1/1.1-1.2/src/components/Header/header.jsx
+++ exercices/module1/1.1-1.2/src/components/Header/header.jsx
@@ -1,6 +1,9 @@
+import logo from './images/LOGO_HE_VINCI.png'
+
 const Header = (props) => {
     return (
       <div>
+        <img src={logo} alt="Logo" />
         <h1>{props.course}</h1>
       </div>
     )
